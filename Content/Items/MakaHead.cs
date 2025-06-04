52331aca606e46019631dd1a96f70ede780b1dcc
Free-Star <77061603+Free-Star@users.noreply.github.com>
2025-06-04 17:23:53 +0800

    fix: add newline at eof for headgear items

--- Content/Items/MakaHead.cs
+++ Content/Items/MakaHead.cs
@@ -6,15 +6,15 @@ namespace FreeStarBro.Content.Items
 {
 	// This tells tModLoader to look for a texture called MinionBossMask_Head, which is the texture on the player
 	// and then registers this item to be accepted in head equip slots
-	[AutoloadEquip(EquipType.Head)]
-	public class MakaHead : ModItem
-	{
-		public override void SetStaticDefaults()
-		{
-			// 强制在所有动作状态下都使用这张贴图
-			ArmorIDs.Head.Sets.DrawHead[Item.headSlot] = true;
-			ArmorIDs.Head.Sets.UseAltFaceHeadDraw[Item.headSlot] = false;
-		}
+       [AutoloadEquip(EquipType.Head)]
+       public class MakaHead : ModItem
+       {
+               public override void SetStaticDefaults()
+               {
+                       // 让头饰在各种动作中都正常显示
+                       ArmorIDs.Head.Sets.DrawHead[Item.headSlot] = false;
+                       ArmorIDs.Head.Sets.UseAltFaceHeadDraw[Item.headSlot] = false;
+               }
 
 		public override void SetDefaults() {
 			Item.width = 32;
--- Content/Items/YiguHead.cs
+++ Content/Items/YiguHead.cs
@@ -6,12 +6,18 @@ namespace FreeStarBro.Content.Items
 {
 	// This tells tModLoader to look for a texture called MinionBossMask_Head, which is the texture on the player
 	// and then registers this item to be accepted in head equip slots
-	[AutoloadEquip(EquipType.Head)]
-	public class YiguHead : ModItem
-	{
-		public override void SetDefaults() {
-			Item.width = 32;
-			Item.height = 32;
+       [AutoloadEquip(EquipType.Head)]
+       public class YiguHead : ModItem
+       {
+               public override void SetStaticDefaults()
+               {
+                       // 让头饰在各种动作中都正常显示
+                       ArmorIDs.Head.Sets.DrawHead[Item.headSlot] = false;
+                       ArmorIDs.Head.Sets.UseAltFaceHeadDraw[Item.headSlot] = false;
+               }
+               public override void SetDefaults() {
+                       Item.width = 32;
+                       Item.height = 32;
 
 			// Common values for every boss mask
 			Item.rare = ItemRarityID.Blue;
