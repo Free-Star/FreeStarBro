cdb2fdda03cc78eac8f6e647020b981c68422729
Free-Star <77061603+Free-Star@users.noreply.github.com>
2025-06-05 14:50:04 +0800

    新增海洋子世界提示

--- Content/Subworlds/OceanPlayer.cs
+++ Content/Subworlds/OceanPlayer.cs
@@ -8,12 +8,24 @@ namespace FreeStarBro.Content.Subworlds
 {
     public class OceanPlayer : ModPlayer
     {
+        private bool approachMessageShown;
+
         public override void PostUpdate()
         {
             int tileX = (int)(Player.position.X / 16f);
 
             if (!SubworldSystem.IsActive<OceanSubworld>())
             {
+                if (!approachMessageShown && tileX >= Main.maxTilesX - 50 && Main.myPlayer == Player.whoAmI)
+                {
+                    Main.NewText(Language.GetTextValue("Mods.FreeStarBro.OceanSubworld.ApproachMessage"), Color.Aqua);
+                    approachMessageShown = true;
+                }
+                if (tileX < Main.maxTilesX - 60)
+                {
+                    approachMessageShown = false;
+                }
+
                 if (tileX >= Main.maxTilesX - 2 && Main.myPlayer == Player.whoAmI)
                 {
                     Main.NewText(Language.GetTextValue("Mods.FreeStarBro.OceanSubworld.EnterMessage"), Color.Aqua);
